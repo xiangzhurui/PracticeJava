e7e28c2a99aa002e0609f17d382cf49318ac2ceb
xiangzhurui <xiangzhurui@gmail.com>
2016-12-03 00:15:47 +0800

    重构properites示例，添加AES对称加密类声明

---
+++ src/main/java/algorithms/crypt/AESCrypt.java
@@ -0,0 +1,44 @@
+package algorithms.crypt;
+
+import java.security.spec.AlgorithmParameterSpec;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+/**
+ * AES 对称加密
+ * 
+ * @author XiangZhuRui
+ *
+ */
+public class AESCrypt {
+
+    // 秘钥
+    private String key;
+
+    public static void main(String[] args) {
+    }
+
+    /**
+     * 加密
+     * 
+     * @param s
+     * @return
+     */
+    public static String encode(String s) {
+        return null;
+
+    }
+
+    /**
+     * 解密
+     * 
+     * @param s
+     * @return
+     */
+    public static String decode(String s) {
+        return null;
+
+    }
+
+}
