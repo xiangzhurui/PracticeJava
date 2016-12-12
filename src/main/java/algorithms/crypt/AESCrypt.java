315583908b725b83211e83e9d639b56ecac075bc
xiangzhurui <xiangzhurui@gmail.com>
2016-12-12 22:46:23 +0800

    AES 对称加密与进制转换
    byte[] 与Hex String  转换方法

--- src/main/java/algorithms/crypt/AESCrypt.java
+++ src/main/java/algorithms/crypt/AESCrypt.java
@@ -1,44 +1,47 @@
 package algorithms.crypt;
 
-import java.security.spec.AlgorithmParameterSpec;
+import java.security.SecureRandom;
+
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
-import javax.crypto.spec.IvParameterSpec;
+
+import basic.ByteArrayToHexString;
 
 /**
- * AES 对称加密
+ * AES symmetric encryption algorithm
  * 
  * @author XiangZhuRui
  *
  */
 public class AESCrypt {
-
-    // 秘钥
-    private String key;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String content = "哈哈哈";
+        String encryptKey = "sfdasSFDS123";
+        byte[] data = aesEncryptToBytes(content, encryptKey);
+        System.out.println(ByteArrayToHexString.bytesToHexString(data));
+//        3394dad0600e228ff09c904af804f594
+        System.out.println(aesDecryptByBytes(ByteArrayToHexString.hexStringToBytes("3394dad0600e228ff09c904af804f594"), encryptKey));
     }
 
-    /**
-     * 加密
-     * 
-     * @param s
-     * @return
-     */
-    public static String encode(String s) {
-        return null;
+    public static byte[] aesEncryptToBytes(String content, String encryptKey) throws Exception {
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        kgen.init(128, new SecureRandom(encryptKey.getBytes()));
 
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(kgen.generateKey().getEncoded(), "AES"));
+
+        return cipher.doFinal(content.getBytes("utf-8"));
     }
 
-    /**
-     * 解密
-     * 
-     * @param s
-     * @return
-     */
-    public static String decode(String s) {
-        return null;
+    public static String aesDecryptByBytes(byte[] encryptBytes, String decryptKey) throws Exception {
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        kgen.init(128, new SecureRandom(decryptKey.getBytes()));
 
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(kgen.generateKey().getEncoded(), "AES"));
+        byte[] decryptBytes = cipher.doFinal(encryptBytes);
+
+        return new String(decryptBytes);
     }
-
 }
