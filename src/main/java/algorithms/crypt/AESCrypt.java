a2b4ad0c2651d4255681e5468100ba6a12e4231c
xiangzhurui <xiangzhurui@gmail.com>
2017-01-05 22:18:12 +0800

    使用JDK实现的AES对称加密算法使用实例
    AES/ECB/PKCS5PADDING

--- src/main/java/algorithms/crypt/AESCrypt.java
+++ src/main/java/algorithms/crypt/AESCrypt.java
@@ -3,6 +3,7 @@ package algorithms.crypt;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Locale;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -11,58 +12,130 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * AES symmetric encryption algorithm
+ * AES 对称加密
  * 
+ * <pre>
+ * String keyStr = "0123456789abcdef";
+ * String content = "hello world";
+ * byte[] temp0 = AESCrypt.enCrypt(content, keyStr);
+ * String secretStr = toHex(temp0);
+ * System.out.println("加密后得到的密文：" + secretStr);
+ * byte[] encodeStr = AESCrypt.deCrypt(secretStr, keyStr);
+ * System.out.println("解密后得到的原文：" + new String(encodeStr));
+ * </pre>
+ * 
+ * @see {@link https://docs.oracle.com/javase/7/docs/api/javax/crypto/Cipher.html}
  * @author XiangZhuRui
- *
  */
 public class AESCrypt {
-	public static void main(String[] args) {
-		String content = "123";
+	private static final Logger log = LoggerFactory.getLogger(AESCrypt.class);
+	private static final String TRANSFORMATION = "AES/ECB/PKCS5PADDING";
+	private static final String HEXSTR = "0123456789ABCDEF";
 
-		String password = "abcdefghabcdefgh";
+	/**
+	 * 加密
+	 * 
+	 * @param bytes
+	 * @param privateKey 长度为16
+	 * @return
+	 */
+	public static byte[] enCrypt(byte[] bytes, byte[] privateKey) {
+		byte[] result = null;
+		try {
+			KeyGenerator kgen = KeyGenerator.getInstance("AES");
+			kgen.init(128, new SecureRandom(privateKey));
+			SecretKeySpec secretKey = new SecretKeySpec(privateKey, "AES");
+			// SecretKey secretKey = getKey(privateKey);
 
-		System.out.println("加密前明文：" + content);
-		System.out.println("password:" + password);
-
-		byte[] encryptResult = encrypt(content.getBytes(), password.getBytes());
-		System.out.println("getBytes 加密后：" + BinaryToHexString(encryptResult));
+			Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+			log.info("encry mode:" + TRANSFORMATION);
+			// cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			result = cipher.doFinal(bytes);
+		} catch (NoSuchAlgorithmException e) {
+			log.error("", e);
+		} catch (NoSuchPaddingException e) {
+			log.error("", e);
+		} catch (InvalidKeyException e) {
+			log.error("", e);
+		} catch (IllegalBlockSizeException e) {
+			log.error("", e);
+		} catch (BadPaddingException e) {
+			log.error("", e);
+		}
+		return result;
 	}
 
-	private static String TRANSFORMATION = "AES/ECB/PKCS5PADDING";
+	/**
+	 * 加密
+	 * 
+	 * @param bytes
+	 * @param privateKey 16个字符
+	 * @return
+	 */
+	public static byte[] enCrypt(String bytes, String privateKey) {
+		return enCrypt(bytes.getBytes(), privateKey.getBytes());
+	}
 
-	private static String hexStr = "0123456789ABCDEF";
-
-	private static final char[] map = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
-			'F' };
-
-	public static byte[] encrypt(byte[] bytes, byte[] password) {
+	/**
+	 * 解密
+	 * 
+	 * @param content
+	 * @param privateKey
+	 * @return
+	 */
+	public static byte[] deCrypt(byte[] content, byte[] privateKey) {
+		log.debug("参数为[{}][{}]", content, privateKey);
+		byte[] result = null;
 		try {
-			KeyGenerator kgen = KeyGenerator.getInstance("AES"); // KeyGenerator提供（对称）密钥生成器的功能。使用getInstance
-			// 类方法构造密钥生成器。
-			kgen.init(256, new SecureRandom(password));// 使用用户提供的随机源初始化此密钥生成器，使其具有确定的密钥大小。
-			SecretKeySpec key = new SecretKeySpec(password, "AES");// 使用SecretKeySpec类来根据一个字节数组构造一个
-			// SecretKey,，而无须通过一个（基于
-			// provider
-			// 的）SecretKeyFactory.
-			Cipher cipher = Cipher.getInstance(TRANSFORMATION);// 创建密码器 //为创建
-			// Cipher
-			// 对象，应用程序调用
-			// Cipher 的
-			// getInstance
-			// 方法并将所请求转换
-			// 的名称传递给它。还可以指定提供者的名称（可选）。
+			KeyGenerator kgen = KeyGenerator.getInstance("AES");
+			kgen.init(128, new SecureRandom(privateKey));
+			SecretKeySpec secretKey = new SecretKeySpec(privateKey, "AES");
+			log.debug("privateKey数组长{}", privateKey.length);
+			// SecretKey secretKey = getKey(privateKey);
+			Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+			log.info("encry mode:" + TRANSFORMATION);
+			cipher.init(Cipher.DECRYPT_MODE, secretKey);
+			result = cipher.doFinal(content);
+		} catch (NoSuchAlgorithmException e) {
+			log.error("", e);
+		} catch (NoSuchPaddingException e) {
+			log.error("", e);
+		} catch (IllegalBlockSizeException e) {
+			log.error("", e);
+		} catch (BadPaddingException e) {
+			log.error("", e);
+		} catch (InvalidKeyException e) {
+			log.error("", e);
+		}
+		return result;
+	}
 
-			System.out.println("encry mode:" + TRANSFORMATION);
+	/**
+	 * 解密
+	 * 
+	 * @param content 十六进制的密文字符串
+	 * @param privateKey 长度为16的秘钥字符串
+	 * @return
+	 */
+	public static byte[] deCrypt(String content, String privateKey) {
+		return deCrypt(toByteArray(content), privateKey.getBytes());
+	}
 
-			byte[] byteContent = bytes;
-
-			cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
-			byte[] result = cipher.doFinal(byteContent); // 按单部分操作加密或解密数据，或者结束一个多部分操作。数据将被加密或解密（具体取决于此
-															// Cipher 的初始化方式）。
-
-			return result; // 加密
+	public static byte[] decrypt(byte[] content, String password) {
+		try {
+			KeyGenerator kgen = KeyGenerator.getInstance("AES");
+			kgen.init(128, new SecureRandom(password.getBytes()));
+			SecretKeySpec key = new SecretKeySpec(password.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+			System.out.println("blockSize:" + cipher.getBlockSize());
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			byte[] result = cipher.doFinal(content);
+			return result;
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		} catch (NoSuchPaddingException e) {
@@ -77,27 +150,54 @@ public class AESCrypt {
 		return null;
 	}
 
-	public static String BinaryToHexString(byte[] bytes) {
-
+	/**
+	 * byte数组转十六进制字符串
+	 * 
+	 * @param bytes[]
+	 * @return
+	 */
+	public static String toHex(byte[] bytes) {
 		String result = "";
 		String hex = "";
 		for (int i = 0; i < bytes.length; i++) {
 			// 字节高4位
-			hex = String.valueOf(hexStr.charAt((bytes[i] & 0xF0) >> 4));
+			hex = String.valueOf(HEXSTR.charAt((bytes[i] & 0xF0) >> 4));
 			// 字节低4位
-			hex += String.valueOf(hexStr.charAt(bytes[i] & 0x0F));
+			hex += String.valueOf(HEXSTR.charAt(bytes[i] & 0x0F));
 			result += hex;
 		}
 		return result;
 	}
 
-	public static String getHexString(byte[] input) {
-		char[] result = new char[input.length * 2];
-		for (int i = 0; i < input.length; i++) {
-			result[i * 2] = map[(input[i] & 240) >>> 4];
-			result[i * 2 + 1] = map[input[i] & 15];
+	/**
+	 * 十六进制字符串转byte数组
+	 * 
+	 * @param hexStr
+	 * @return
+	 */
+	public static byte[] toByteArray(String hexStr) {
+		/* 对输入值进行规范化整理 */
+		hexStr = hexStr.trim().replace(" ", "").toUpperCase(Locale.US);
+		// 处理值初始化
+		int m = 0, n = 0;
+		int iLen = hexStr.length() / 2;
+		byte[] ret = new byte[iLen];
+
+		for (int i = 0; i < iLen; i++) {
+			m = i * 2 + 1;
+			n = m + 1;
+			ret[i] = (byte) (Integer.decode("0x" + hexStr.substring(i * 2, m) + hexStr.substring(m, n)) & 0xFF);
 		}
-		return new String(result);
+		return ret;
 	}
 
+	/**
+	 * byte数组转回普通的char[]的String
+	 * 
+	 * @param bytes
+	 * @return
+	 */
+	public static String toString(byte[] bytes) {
+		return new String(bytes);
+	}
 }
