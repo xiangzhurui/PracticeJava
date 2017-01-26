ed89ff670eafbb0f887bd05df85bf5f6752711c6
xiangzhurui <xiangzhurui@gmail.com>
2017-01-26 10:17:40 +0800

    使用commons codec 进行Base64编码解码

--- src/main/java/sample/commons/csv/CsvBuilder.java
+++ src/main/java/sample/commons/csv/CsvBuilder.java
@@ -4,70 +4,53 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
 public class CsvBuilder {
-	private static final Object[] header = { "id", "name" };
+	private static final String LINE_SEPARATOR = "\n";
 
-	private static FileWriter fileWriter = null;
-	private static CSVPrinter csvPrinter = null;
-	private static CSVFormat csvFormat = CSVFormat.DEFAULT.withRecordSeparator("\r\n"); // 每条记录间隔符
+	public static void main(String[] args) {
+	}
 
-	public static void write(Map<String, String> info) {
-		File file = new File("test.csv");
-		System.out.println(file.getAbsolutePath());
+	/**
+	 * 写入csv文件
+	 *
+	 * @param file 传入文件的路径，包含文件名
+	 */
+	private <T> void csvWriter(File file, String[] File_HEAND, List<T> dataLines) {
+		FileWriter fileWriter;
+		CSVPrinter csvPrinter = null;
+		CSVFormat csvFormat = CSVFormat.DEFAULT.withRecordSeparator(LINE_SEPARATOR);
 		try {
-			if (!file.exists()) {
-				fileWriter = new FileWriter("test.csv", true); // 创建test.csv的字符输出流
-				csvPrinter = new CSVPrinter(fileWriter, csvFormat);
-				csvPrinter.printRecords(header); // 生成.csv表的字段名
-				System.out.println("执行");
-			} else {
-				fileWriter = new FileWriter("test.csv", true); // 创建test.csv的字符输出流
-				csvPrinter = new CSVPrinter(fileWriter, csvFormat);
-				System.out.println("文件存在");
+			fileWriter = new FileWriter(file);
+			csvPrinter = new CSVPrinter(fileWriter, csvFormat);
+			csvPrinter.printRecord((Object[]) File_HEAND);
+			for (T t : dataLines) {
+				Class<? extends Object> clazz = t.getClass();
+				List<String> cityPriceRecord = new ArrayList<String>();
+				csvPrinter.printRecord(cityPriceRecord);
 			}
-
-			Set<String> ids = info.keySet();
-			List<String> idName = new ArrayList<String>();
-			for (String id : ids) {
-				
-				idName.add(id);
-				idName.add(info.get(id));
-			}
-			
-			csvPrinter.printRecord(idName); // 向.csv文件中添加记录数据
-			System.out.println("生成.csv文件");
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				csvPrinter.flush();
-				fileWriter.flush();
-				fileWriter.close();
-				csvPrinter.close();
-			} catch (Exception e) {
+				if (csvPrinter != null) {
+					csvPrinter.flush();
+					csvPrinter.close();
+				}
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-
 	}
 
-	public static void main(String[] args) {
+	public static String captureName(String s) {
+		char[] cs = s.toCharArray();
+		cs[0] -= 32;
+		return String.valueOf(cs);
 
-		Map<String, String> idName = new HashMap<String, String>();
-		idName.put("1", "Java");
-		idName.put("2", "C++");
-		idName.put("3", "Python");
-		idName.put("4", "Ruby");
-		write(idName);
 	}
 }
