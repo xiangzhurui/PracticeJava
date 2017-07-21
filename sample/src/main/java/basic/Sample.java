5d903c82e5aec64999f03a346332a3ba09cac74c
xiangzhurui <xiangzhurui@gmail.com>
2017-07-21 15:03:41 +0800

    dbcp2参考配置，规避MySQL8小时问题与连接数无法释放

--- sample/src/main/java/basic/Sample.java
+++ sample/src/main/java/basic/Sample.java
@@ -1,23 +1,45 @@
 package basic;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sample {
 
-	public static void main(String[] args) {
-		long x = 1L;
-		BigInteger bi = new BigInteger("1");
-		System.out.println(bi.shiftLeft(99));
-		int a = 4;
-		System.out.println(one(a));
-		System.out.println(two(a));
-	}
+    public static void main(String[] args) {
+        List list = new ArrayList();
 
-	static int one(int a) {
-		return ++a + a;
-	}
+        int size = list.size();
+        int start = 0;
+        int num = 500;
+        //分段
+        while (start < size) {
+            int end = start + num < size ? start + num : size - 1;
+            List sub = list.subList(start, end);
+            //do ....
+            if (end == size - 1) {
+                break;
+            } else {
+                start = end;
+            }
+        }
 
-	static int two(int a) {
-		return a + a++;
-	}
+    }
+
+    public static void testInt() {
+        long x = 1L;
+        BigInteger bi = new BigInteger("1");
+        System.out.println(bi.shiftLeft(99));
+        int a = 4;
+        System.out.println(one(a));
+        System.out.println(two(a));
+    }
+
+    public static int one(int a) {
+        return ++a + a;
+    }
+
+    public static int two(int a) {
+        return a++ + a;
+    }
 }
