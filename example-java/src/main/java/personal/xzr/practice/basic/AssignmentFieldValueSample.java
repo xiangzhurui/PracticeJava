97dd534e4965cd547de59ba787a4b1915d76da7f
xiang.zhurui <xiangzhurui@gmail.com>
2017-08-25 17:20:01 +0800

    Java 对象属性赋值方式示例。

--- example-java/src/main/java/personal/xzr/practice/basic/AssignmentFieldValueSample.java
+++ example-java/src/main/java/personal/xzr/practice/basic/AssignmentFieldValueSample.java
@@ -1,35 +1,34 @@
 package personal.xzr.practice.basic;
 
-public class Test2 {
-	//实例field，在申明时赋值了
+public class AssignmentFieldValueSample {
+    //实例field，在申明时赋值了
     private final String str = "Java";
-    
+
     //实例field，在申明时未赋值
     private final String str2;
-    
+
     //实例field，在申明时未赋值
     private final int i;
-    
+
     //类field，在申明时赋值了
     private static final int i2 = 2;
-    
+
     private static final int i3;
-    
+
     {
         //在普通初始化块中给实例field赋值
         str2 = "normalFinalField";
     }
-    
-    static
-    {
+
+    static {
         //在静态初始化块中给类field指定初始值
         i3 = 3;
     }
-    public Test2()
-    {
+
+    public AssignmentFieldValueSample() {
         //在构造器中给实例field赋值
-        i = 1;        
-        
+        i = 1;
+
         //在构造器中给已经在初始化中赋了值的str2重新赋值，将会报编译错。
         //str2 = "re";
     }
