ef493a461227f5b8d281253accf91d326212e12d
xiang.zhurui <xiangzhurui@gmail.com>
2017-09-29 10:54:45 +0800

    drools 相关。

---
+++ drools/src/main/java/com/xiangzhurui/config/DroolsCongfig.java
@@ -0,0 +1,81 @@
+package com.xiangzhurui.config;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.*;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
+import org.kie.spring.KModuleBeanFactoryPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Drools Spring 集成配置
+ *
+ * @author xiangzhurui
+ * @version 2017/9/29
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.xiangzhurui.service.drools"})
+public class DroolsCongfig {
+
+    private static final String RULES_PATH = "rules";
+
+    @Bean
+    public KieFileSystem kieFileSystem() throws IOException {
+        KieFileSystem kieFileSystem = getKieServices().newKieFileSystem();
+        for (Resource file : getRuleFiles()) {
+            kieFileSystem.write(ResourceFactory.newClassPathResource(RULES_PATH + file.getFilename(), "UTF-8"));
+        }
+        return kieFileSystem;
+    }
+
+
+    @Bean
+    public KieContainer kieContainer() throws IOException {
+        final KieRepository kieRepository = getKieServices().getRepository();
+
+        kieRepository.addKieModule(new KieModule() {
+            public ReleaseId getReleaseId() {
+                return kieRepository.getDefaultReleaseId();
+            }
+        });
+
+        KieBuilder kieBuilder = getKieServices().newKieBuilder(kieFileSystem());
+        kieBuilder.buildAll();
+
+        return getKieServices().newKieContainer(kieRepository.getDefaultReleaseId());
+    }
+
+    @Bean
+    public KieBase kieBase() throws IOException {
+        return kieContainer().getKieBase();
+    }
+
+    @Bean
+    public KieSession kieSession() throws IOException {
+        return kieContainer().newKieSession();
+    }
+
+    @Bean
+    public KModuleBeanFactoryPostProcessor kiePostProcessor() {
+        return new KModuleBeanFactoryPostProcessor();
+    }
+
+    private Resource[] getRuleFiles() throws IOException {
+        ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+        return resourcePatternResolver.getResources("classpath*:" + RULES_PATH + File.separator + "**/*.*");
+    }
+
+    private KieServices getKieServices() {
+        return KieServices.Factory.get();
+    }
+}
---
+++ drools/src/test/java/ParamsterTest.java
@@ -0,0 +1,7 @@
+/**
+ * @author xiangzhurui
+ * @version 2017/9/26
+ */
+public class ParamsterTest {
+
+}
