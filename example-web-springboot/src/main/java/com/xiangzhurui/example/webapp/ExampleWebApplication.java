13c47f58c3645e0e9c80f5cc83a284550686dc6d
xiangzhurui <xiangzhurui@gmail.com>
2017-10-19 21:59:17 +0800

    add web project with spring boot.

---
+++ example-web-springboot/src/main/java/com/xiangzhurui/example/webapp/ExampleWebApplication.java
@@ -0,0 +1,12 @@
+package com.xiangzhurui.example.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleWebApplication.class, args);
+	}
+}
