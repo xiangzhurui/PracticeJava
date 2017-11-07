10a6850be1a86da8cf7952b7ea33c62d624b0a11
xiangzhurui <xiangzhurui@gmail.com>
2017-11-07 22:33:17 +0800

    fix

--- drools/src/test/java/com/xiangzhurui/service/DroolsTest.java
+++ drools/src/test/java/com/xiangzhurui/service/DroolsTest.java
@@ -126,7 +126,7 @@ public class DroolsTest {
         // 通过 kContainer 获取 kmodule.xml 中定义的 ksession
         KieSession kieSession = kieContainer.newKieSession("ksession-rules");
         // 向 workingMemory 中加入一个对象
-        kieSession.insert"Tom");
+        kieSession.insert("Tom");
         // 通知规则引擎执行规则
         kieSession.fireAllRules();
     }
