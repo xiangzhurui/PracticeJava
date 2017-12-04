69f25102f21d991921a87495ad10d226574aec53
xiangzhurui <xiangzhurui@gmail.com>
2017-12-04 15:42:25 +0800

    guava cache

--- util/src/main/java/personal/xzr/practice/util/cache/GuavaCache.java
+++ util/src/main/java/personal/xzr/practice/util/cache/GuavaCache.java
@@ -1,20 +1,13 @@
 package personal.xzr.practice.util.cache;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-
-import com.google.common.base.MoreObjects;
-import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListenableFutureTask;
-import lombok.ToString;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author xiangzhurui
@@ -28,29 +21,29 @@ public class GuavaCache {
             .refreshAfterWrite(5, TimeUnit.SECONDS)
             .build(
                     new CacheLoader<String, String>() {
+                        @Override
                         public String load(String key) { // no checked exception
-                            return getGraphFromDatabase(key);
+                            return loadDataFromDb(key);
                         }
 
+                        @Override
                         public ListenableFuture<String> reload(final String key, String prevGraph) {
                             if (neverNeedsRefresh(key)) {
                                 return Futures.immediateFuture(prevGraph);
                             } else {
                                 // asynchronous!
-                                return ListenableFutureTask.create(new Callable<String>() {
-                                    public String call() {
-                                        return getGraphFromDatabase(key);
-                                    }
-                                });
+                                return ListenableFutureTask.create(() -> loadDataFromDb(key));
                             }
                         }
                     });
 
     public static boolean neverNeedsRefresh(String Key) {
+        //TODO
         return false;
     }
 
-    public static String getGraphFromDatabase(String key) {
+    public static String loadDataFromDb(String key) {
+        //TODO
         return null;
     }
 }
