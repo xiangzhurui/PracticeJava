1a2f3ac75fe9d86293b65aeea48cea5311db7b5a
xiangzhurui <xiangzhurui@gmail.com>
2018-03-16 17:30:36 +0800

    update drools version to 7.6.0.Final

---
+++ drools/src/test/java/com/xzr/drools/ImportsTest.java
@@ -0,0 +1,30 @@
+package com.xzr.drools;
+
+import lombok.extern.slf4j.Slf4j;
+import org.drools.workbench.models.datamodel.rule.RuleAttribute;
+import org.kie.soup.project.datamodel.imports.Import;
+import org.kie.soup.project.datamodel.imports.Imports;
+
+/**
+ * @author xiangzhurui
+ * @version 2018/3/16
+ */
+@Slf4j
+public class ImportsTest {
+    public static void main(String[] args) {
+        Imports imports = new Imports();
+
+        Import item = new Import(Long.class);
+
+        log.info("item:[{}]", item.getType());
+
+        imports.addImport(item);
+
+        log.info("imports:[{}]", imports);
+
+
+        RuleAttribute ruleAttribute = new RuleAttribute("no-loop","true");
+        log.info(ruleAttribute.toString());
+
+    }
+}
